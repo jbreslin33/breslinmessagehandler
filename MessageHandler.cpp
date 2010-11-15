383c5464bdfe84e89f3aa5c06df490cd94c4959f
jbreslin33 <jbreslin33@localhost>
2010-11-15 11:36:07 -0500

    ..

--- MessageHandler.cpp
+++ MessageHandler.cpp
@@ -19,14 +19,4 @@ MessageHandler::~MessageHandler(void)
 /*this should call a function on the client or the server */
 void MessageHandler::translateMessage(char* message)
 {
-/*
-	if (message[0] == "u")
-	{
-		std::cout << "call username function" << std::endl;
-	}
-	else
-	{
-		std::cout << "call something" << std::endl;
-	}
-*/
 }
