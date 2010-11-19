74957e08183bd72c222f31c4c6172df99168bd90
jbreslin33 <jbreslin33@localhost>
2010-11-19 15:08:18 -0500

    ...

--- MessageHandler.cpp
+++ MessageHandler.cpp
@@ -14,12 +14,8 @@ MessageHandler::MessageHandler()
 {
 	//create ListenServer here as ListenServer has no need to be subclassed for different games.
 	std::cout << "MessageHandler Constructor\n";
-	ListenServer* ls = new ListenServer();
 
-	std::cout << "setListenServer(ls) from inside MessageHandler Constructor\n";
-	setListenServer(ls);
-	
-	//I won't pass in MessageHandler to ListenServer I will let the subclass particular to the game do it.
+	mListenServer = NULL;	
 
 }
 //-------------------------------------------------------------------------------------
